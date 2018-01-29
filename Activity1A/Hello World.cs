e6836bd3a9b7c5cc304986daa393c40cca032a7e
access2purnima <access2purnima@users.noreply.github.com>
2018-01-29 17:19:51 +0000

    removed unwanted files

--- Activity1A/Hello World.cs
+++
@@ -1,9 +0,0 @@
-﻿class Program
-{
-    static void Main()
-    {
-        System.Console.WriteLine("Hello World!");
-        System.Console.ReadKey();
-    }
-}
-
--- Activity1A/Press any key to exit.cs
+++
@@ -1,8 +0,0 @@
-﻿class Program
-{
-    static void Main()
-    {
-        System.Console.WriteLine("Press any key to exit");
-        System.Console.ReadKey();
-    }
-}
